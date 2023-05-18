000eda31023ef59427232ff8d85ec2d2d790de62
Rafal Zdunek <rafal93zdunek@gmail.com>
2023-05-18 18:30:06 +0200

    add page loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import './App.styled'
+import React, {Suspense} from 'react';
+import foodbg from './assets/foodbg.mp4';
 import Form from "./components/Form/Form";
 import { ToastContainer } from 'react-toastify';
-import foodbg from './assets/foodbg.mp4';
+import PageLoader from './components/PageLoader/PageLoader';
 import { Video, Content, Container, Background } from './App.styled';
 import "react-toastify/dist/ReactToastify.min.css";
+import './App.styled'
 
 
 const App:React.FC = () => {
 
   return (
-    <Container>
-      <Video autoPlay loop muted>
-        <Background src={foodbg} type='video/mp4' />
-      </Video>
-      <Content>
-        <Form/>
-      </Content>
+    <Suspense fallback={<PageLoader/>}>
+      <Container>
+        <Video autoPlay loop muted>
+          <Background src={foodbg} type='video/mp4' />
+        </Video>
+        <Content>
+          <Form/>
+        </Content>
+      </Container>
       <ToastContainer />
-    </Container>
+    </Suspense>
+    
   )
 }
 
