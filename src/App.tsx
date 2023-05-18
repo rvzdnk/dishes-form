af946d3c4e7d56da194c890dd12cbe6bea283163
Rafal Zdunek <rafal93zdunek@gmail.com>
2023-05-18 18:43:43 +0200

    remove page loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import foodbg from './assets/foodbg.mp4';
 import Form from "./components/Form/Form";
 import { ToastContainer } from 'react-toastify';
-import PageLoader from './components/PageLoader/PageLoader';
 import { Video, Content, Container, Background } from './App.styled';
 import "react-toastify/dist/ReactToastify.min.css";
-import './App.styled'
+import './App.styled';
 
 
 const App:React.FC = () => {
 
   return (
-    <Suspense fallback={<PageLoader/>}>
       <Container>
         <Video autoPlay loop muted>
           <Background src={foodbg} type='video/mp4' />
@@ -19,10 +17,8 @@ const App:React.FC = () => {
         <Content>
           <Form/>
         </Content>
-      </Container>
       <ToastContainer />
-    </Suspense>
-    
+      </Container>
   )
 }
 
